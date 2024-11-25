fc11bc6634759749751fcf81e055013f26ca9e0c
viniciusfal <vinicius_prsilva@hotmail.com>
2024-11-25 16:27:34 -0300

    feat: railwais

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://erp-1xqz.onrender.com",
+  baseURL: "https://api-erp-amazonia.up.railway.app",
   withCredentials: true,
 })
