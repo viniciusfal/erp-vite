6bb2f337a046fdd2ea03c1c77ffa656dd25a831d
viniciusfal <vinicius_prsilva@hotmail.com>
2025-02-04 13:44:06 -0300

    feat: railways return developing

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://erpnet.up.railway.app/api",
   withCredentials: true,
   timeout: 10000,
 })
